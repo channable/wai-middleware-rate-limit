a0e8ff9a99f75cf6083d5d8c181515f35edbf39a
Robert Kreuzer <robert@channable.com>
2020-05-14 12:17:34 +0200

    Import Frequency.hs
    Imported from Warpmachine. The imported code here was originally written
    by @rightfold and @NunoAlexandre.

---
+++ src/Network/Wai/Middleware/RateLimit/Frequency.hs
@@ -0,0 +1,28 @@
+module Data.Time.Frequency
+  ( Frequency (..)
+  , RequestsPerMinute (..)
+  , fromRequestsPerMinute
+  , fromHz
+  , mulDxF
+  ) where
+
+import Data.Time.Clock (NominalDiffTime)
+
+
+-- | A frequency is represented by its reciprocal. For example, @Frequency 0.2@
+-- represents a frequency of 5 Hz.
+newtype Frequency = Frequency { freqRecip :: NominalDiffTime }
+  deriving (Eq, Ord, Show)
+
+newtype RequestsPerMinute = RequestsPerMinute Integer
+
+-- | @fromHz n = n Hz@.
+fromHz :: Double -> Frequency
+fromHz = Frequency . fromRational . recip . toRational
+
+fromRequestsPerMinute :: RequestsPerMinute -> Frequency
+fromRequestsPerMinute (RequestsPerMinute n) = fromHz $  fromIntegral n / 60
+
+-- | Multiplying a duration (s) by a frequency (1/s) gives a number.
+mulDxF :: NominalDiffTime -> Frequency -> Rational
+mulDxF d f = toRational $ d / freqRecip f
