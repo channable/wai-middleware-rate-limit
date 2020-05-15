ea8e9881feb7cf4ae4ae86d22547f56998663c4d
Robert Kreuzer <robert@channable.com>
2020-05-15 16:24:19 +0200

    Add an example with a custom token
    This does not work with the library yet.

--- example/Main.hs
+++ example/Main.hs
@@ -1,6 +1,15 @@
 -- This file contains a minimal example of how to use the rate-limiting middleware.
 -- We use an STMMap to persist the rate-limiting state across requests.
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
+import Data.Hashable (Hashable (..))
+import Data.List (find)
+import Data.Text (Text)
+import Data.Text.Encoding (decodeUtf8)
+
+import Network.HTTP.Types (hAuthorization)
+import Network.Wai (Request, requestHeaders)
 import Network.Wai.Application.Static (staticApp, defaultWebAppSettings)
 import Network.Wai.Handler.Warp (run)
 import Network.Wai.Middleware.RequestLogger (logStdoutDev)
@@ -8,6 +17,15 @@ import Network.Wai.Middleware.RateLimit (
     LeakyBucketSpec (..), RateLimitState (..), fromHz, inMemory, rateLimitMiddleware)
 
 import qualified StmContainers.Map as STMMap
+
+-- your token type must be deriving Hashable, since the hash of the token data will
+-- be used as the key for the rate limiting hash map
+newtype MyToken = MyToken {getText :: Text} deriving (Eq, Ord, Show, Hashable)
+
+extractToken :: Request -> Maybe MyToken
+extractToken request = maybe Nothing (Just . MyToken . decodeUtf8 . snd) maybeHeader
+  where
+    maybeHeader = find ((== hAuthorization) . fst) (requestHeaders request)
 
 
 main :: IO ()
@@ -33,8 +51,10 @@ main = do
         -- the concrete token type here.
         middleware = rateLimitMiddleware rateLimitState Nothing
 
+        app = logStdoutDev $ middleware $ staticApp $ defaultWebAppSettings "."
+
     -- Start a web server, serving static files from the local directory.
     -- Requests are rate-limited based on IP address, according to the "ipLeakyBucketSpec" above.
     -- You can test this by sending a few requests with "curl localhost:8080" and noticing the
     -- "429 Too Many Requests" responses, that are being logged to stdout.
-    run 8080 (logStdoutDev $ middleware $ staticApp $ defaultWebAppSettings ".")
+    run 8080 app
